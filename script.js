7a0af42fe84007b9604ae4c8809e0d4f893382a3
imrapii <rafiamrullahalbaihaqi@gmail.com>
2024-10-21 15:36:40 +0700

    Attempt 1 - 15.36

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 // TODO: Implement the password generation logic based on user input
-
-const generatePassword = (length, options) => {
+export const generatePassword = (length, options) => {
     // Character sets for password generation
     const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const lowercase = "abcdefghijklmnopqrstuvwxyz";
     const numbers = "0123456789";
     const specialChars = "!@#$%^&*()";
 
-    // TODO: Create a variable for the character set based on selected options
+    let charSet = '';
+    if (options.includeUppercase) charSet += uppercase;
+    if (options.includeLowercase) charSet += lowercase;
+    if (options.includeNumbers) charSet += numbers;
+    if (options.includeSpecialChars) charSet += specialChars;
 
-    // TODO: Generate the password based on the selected criteria
+    if (charSet === '') {
+        throw new Error('At least one character type must be selected.');
+    }
+
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charSet.length);
+        password += charSet[randomIndex];
+    }
+
     return password;
 };
-
-// TODO: Add event listener to the button to call generatePassword and display the output
-
-// BONUS: Implement the copy to clipboard functionality
