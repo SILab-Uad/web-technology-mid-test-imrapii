ccfe060286b90cbd83133fc4555ad7ba82622386
imrapii <rafiamrullahalbaihaqi@gmail.com>
2024-10-22 06:48:51 +0700

    Attempt 5 - 06.46 (Finally)

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-// TODO: Implement the password generation logic based on user input
 export const generatePassword = (length, options) => {
-    // Character sets for password generation
     const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const lowercase = "abcdefghijklmnopqrstuvwxyz";
     const numbers = "0123456789";
     const specialChars = "!@#$%^&*()";
 
-    let charSet = '';
-    if (options.includeUppercase) charSet += uppercase;
-    if (options.includeLowercase) charSet += lowercase;
-    if (options.includeNumbers) charSet += numbers;
-    if (options.includeSpecialChars) charSet += specialChars;
+    let characterSet = '';
+    if (options.includeUppercase) characterSet += uppercase;
+    if (options.includeLowercase) characterSet += lowercase;
+    if (options.includeNumbers) characterSet += numbers;
+    if (options.includeSpecialChars) characterSet += specialChars;
 
-    if (charSet === '') {
-        throw new Error('centang salah satu ya sayang');
+    if (characterSet === '') {
+        throw new Error('At least one character type must be selected.');
     }
 
     let password = '';
     for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * charSet.length);
-        password += charSet[randomIndex];
+        const randomIndex = Math.floor(Math.random() * characterSet.length);
+        password += characterSet[randomIndex];
     }
 
     return password;
